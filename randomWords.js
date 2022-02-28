29bbc86c9235792162281cc7fa34f77dd5e434b8
Masheen1988 <matthew.cox@sumeffect.com>
2022-02-28 01:57:33 -0600

    Added menu to generate more random words.

--- randomWords.js
+++ randomWords.js
@@ -1,33 +1,100 @@
-const words = [];
-let word = words.pop();
-let index = prompt("Select an indexed word 0-9:0");
+//A class containing and object array of randomly generated words - Begin
+let newWordsAPI = randomWordsAPI();
 
-//Fetch Random Words API - Begin
-fetch("https://random-word-api.herokuapp.com/word?number=10")
-  .then((response) => response.json())
-  .then(function (result) {
-    //Logs the fetch results to console.
-    console.log("Original Result Array:", result);
+class randomWordsClass {
+  constructor() {
+    this.randomlyGeneratedWords = [];
+  }
+}
 
-    //Loop to iterate through each  results - Begin
-    for (var i = 0; i < result.length; i++) {
-      words.push(`${[i]} ${result[i]} \n`);
+function randomWordsAPI(blankWordsArr) {
+  //Fetch Random Words API - Begin
+  let index = prompt("Select an indexed word: 0-9");
+
+  let words = [];
+  let newblankWordsArr = [];
+  let getWords = fetch("https://random-word-api.herokuapp.com/word?number=10")
+    .then((response) => response.json())
+    .then(function (result) {
+      //Logs the fetch results to console.
+      console.log("Results inside promise:", result);
+
+      //Loop to iterate through each  results - Begin
+      for (var i = 0; i < result.length; i++) {
+        words.push(`${result[i]}`);
+      }
+
+      /*
+      Logs the returned words to console.
+      console.log("Original Words:", words);
+      */
+
+      //Outputs to console, a word from the selected index.
+      console.log(`
+      Inside promise: You selected index: ${index} and the word returned is - ${result.at(index)}
+      `);
+
+      //Alerts which index and word you chose.
+      // alert(`
+      // Inside promise: Using an index of ${index} the word returned is: ${result[index]}`);
+      //*** Looking to return the api array to a new array to interact with. ***/
+    })
+    //Returns an error if the API is not working properly.` - Begin
+    .catch((error) => {
+      console.log("This is an error:", error);
+    });
+  console.log("Inside Function: Words Generated -", words);
+
+  newblankWordsArr.push(words);
+
+  for (let i = 0; i < newblankWordsArr.length; i++) {
+    return newblankWordsArr[i];
+  }
+}
+
+//Start API
+
+// console.log("You randomly generate array is:", newWordsAPI);
+
+//Random Words Main Menu - Begin
+class mainMenu {
+  constructor() {
+    this.sections = [];
+    this.selectedSection = null;
+  }
+
+  //Displays the mainMenu when requesting to start - Begin
+  start() {
+    //Displays the mainMenu until a selected has been made.
+    let menuSelection = this.displayMainMenuOptions();
+    while (menuSelection != "exit") {
+      console.log("You have chosen:", menuSelection);
+      switch (menuSelection) {
+        case "1":
+          randomWordsAPI();
+          break;
+        default:
+          menuSelection = "exit";
+      }
+      //Keeps looping until 0 is selected.
+      menuSelection = this.displayMainMenuOptions();
     }
-    //Logs the returned words to console.
-    // console.log("Original Words:", words);
+    //If 0 is seledcted the program will exit.
+    alert(`Hope you had fun. Goodbye!`);
+  }
 
-    //Outputs to console, a word from the selected index.
-    console.log(`
-    Using an index of ${index} the word returned is ${result.at(index)}
-    `);
-
-    //Code to execute - Begin
-
-    //Outputs to an alert, a word from the selected index.
-    return alert(`
-    Using an index of ${index} the word returned is: ${result[index]}`);
-  })
-  //Returns an error if the API is not working properly.` - Begin
-  .catch((error) => console.log("error", error));
-
-//Outside of Code
+  //Displays the main menu available options - Begin
+  displayMainMenuOptions() {
+    return prompt(`
+   Generate random words!
+   >----------------------------------------------------<
+  
+      * Type leave, type: exit
+      1) Generate more Words
+      2) Nothing happens yet!
+      `);
+  }
+}
+//Initializes the myMenuApp program
+let promptMenu = new mainMenu();
+promptMenu.start();
